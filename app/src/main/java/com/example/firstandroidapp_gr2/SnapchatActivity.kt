2c2834132adc33a23aaa02de43fa2f28a4c80034
Shqipdon Mehmeti <keds30671@KEDS.kv>
2023-11-14 20:57:29 +0100

    ushtrime

---
+++ app/src/main/java/com/example/firstandroidapp_gr2/SnapchatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firstandroidapp_gr2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SnapchatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snapchat)
+    }
+}
