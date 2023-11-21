b3c853cf7f11fa749ba5f0c77e912c22eff239b5
Shqipdon Mehmeti <keds30671@KEDS.kv>
2023-11-21 20:33:44 +0100

    snap activity

--- app/src/main/java/com/example/firstandroidapp_gr2/SnapchatActivity.kt
+++ app/src/main/java/com/example/firstandroidapp_gr2/SnapchatActivity.kt
@@ -1,11 +1,33 @@
 package com.example.firstandroidapp_gr2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 
 class SnapchatActivity : AppCompatActivity() {
+    private lateinit var btnLogin: AppCompatButton
+    private lateinit var btnSignup: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snapchat)
+        findViews()
+        onButtonClicks()
+
+    }
+
+    private fun onButtonClicks() {
+        btnLogin.setOnClickListener {
+            Toast.makeText(this, "Login was clicked", Toast.LENGTH_LONG).show()
+        }
+        btnSignup.setOnClickListener {
+            Toast.makeText(this, "Sign up was clicked", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun findViews() {
+        btnLogin = findViewById(R.id.btnSnapLogin)
+        btnSignup = findViewById(R.id.btnSnapSignUp)
     }
 }
