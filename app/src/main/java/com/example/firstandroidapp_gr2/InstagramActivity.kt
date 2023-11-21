b4a2bc931d5dc45ebaad5fb583410a7ade2e14d6
Shqipdon Mehmeti <keds30671@KEDS.kv>
2023-11-21 20:02:46 +0100

    instagram logic

--- app/src/main/java/com/example/firstandroidapp_gr2/InstagramActivity.kt
+++ app/src/main/java/com/example/firstandroidapp_gr2/InstagramActivity.kt
@@ -2,10 +2,27 @@ package com.example.firstandroidapp_gr2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.textfield.TextInputLayout
 
 class InstagramActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instagram)
+        val btnSignIn : AppCompatButton = findViewById(R.id.btnSignIn)
+        val etUsername : TextInputLayout = findViewById(R.id.username)
+        val etPassword : TextInputLayout = findViewById(R.id.password)
+        btnSignIn.setOnClickListener {
+            val usernameValue = etUsername.editText?.text.toString()
+            val passwordValue = etPassword.editText?.text.toString()
+            if (usernameValue.isNullOrEmpty() || passwordValue.isNullOrEmpty()) {
+                Toast.makeText(this,"Invalid login",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,"Valid login",Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/firstandroidapp_gr2/TipAmountActivity.kt
+++ app/src/main/java/com/example/firstandroidapp_gr2/TipAmountActivity.kt
@@ -2,10 +2,45 @@ package com.example.firstandroidapp_gr2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.SwitchCompat
+import kotlin.math.ceil
 
 class TipAmountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tip_amount)
+        val etCostOfService : EditText = findViewById(R.id.etCostOfService)
+        val rgServiceOptions : RadioGroup = findViewById(R.id.rgServiceOptions)
+        val swRoundUp : SwitchCompat = findViewById(R.id.swRoundUpTip)
+        val btnCalculate : AppCompatButton = findViewById(R.id.btnCalculate)
+        val tvTipAmount : TextView = findViewById(R.id.tvTipAmount)
+
+        btnCalculate.setOnClickListener {
+            val costOfServiceValue = etCostOfService.text.toString().toDoubleOrNull()
+            var result : Double? = 0.0
+            result = when (rgServiceOptions.checkedRadioButtonId) {
+                R.id.rbAmazing -> {
+                    costOfServiceValue?.times(0.20)
+                }
+                R.id.rbGood -> {
+                    costOfServiceValue?.times(0.18)
+                }
+                R.id.rbOkay -> {
+                    costOfServiceValue?.times(0.15)
+                }
+                else -> 0.0
+            }
+            if (swRoundUp.isChecked) {
+                tvTipAmount.text = "Tip amount after calculation is ${ceil(result ?: 0.0)}"
+            }
+            else {
+                tvTipAmount.text = "Tip amount after calculation is $result"
+            }
+        }
     }
 }
